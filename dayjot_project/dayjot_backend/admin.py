b9989411a2f18ee04e520acfc221e0a8eea58682
meixingc <realmeixing@gmail.com>
2023-05-18 18:00:16 -0400

    admin setup

--- dayjot_project/dayjot_backend/admin.py
+++ dayjot_project/dayjot_backend/admin.py
@@ -1,1 +1,10 @@
 from django.contrib import admin
+from .models import User, Entry, Water, Food, Exercise, Sleep, Weight
+
+admin.site.register(User)
+admin.site.register(Entry)
+admin.site.register(Water)
+admin.site.register(Food)
+admin.site.register(Exercise)
+admin.site.register(Sleep)
+admin.site.register(Weight)
