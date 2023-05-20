71fce665652f4d66c3eec54365c3d69193deb7ac
meixingc <realmeixing@gmail.com>
2023-05-20 15:47:26 -0400

    large model edits + migrations

--- dayjot_project/dayjot_backend/models.py
+++ dayjot_project/dayjot_backend/models.py
@@ -3,8 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     picture = models.TextField(default='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png')
-    weight = models.IntegerField(null=True, blank=True)
-    height = models.IntegerField(null=True, blank=True)
     def __str__(self):
         return self.username
 
@@ -21,33 +19,33 @@ class Water(models.Model):
     entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
     amount = models.IntegerField(null=False, blank=False)
     def __str__(self):
-        return str(self.entry.date)
+        return str(self.entry.user) + ' ' + str(self.entry.date)
 
 class Food(models.Model):
     entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, null=False, blank=False)
-    amount = models.CharField(null=False, blank=False)
-    calories = models.IntegerField(null=False, blank=False)
+    amount = models.CharField(max_length=50, null=True, blank=True)
+    calories = models.IntegerField(null=True, blank=True)
     def __str__(self):
-        return str(self.entry.date)
+        return str(self.entry.user) + ' ' + str(self.entry.date)
 
 class Exercise(models.Model):
     entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, null=False, blank=False)
     duration = models.IntegerField(null=False, blank=False)
-    calories = models.IntegerField(null=False, blank=False)
-    links = models.CharField(max_length=1000)
+    calories = models.IntegerField(null=True, blank=True)
+    links = models.CharField(max_length=1000, null=True, blank=True)
     def __str__(self):
-        return str(self.entry.date)
+        return str(self.entry.user) + ' ' + str(self.entry.date)
 
 class Sleep(models.Model):
     entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
     duration = models.IntegerField(null=False, blank=False)
     def __str__(self):
-        return str(self.entry.date)
+        return str(self.entry.user) + ' ' + str(self.entry.date)
 
 class Weight(models.Model):
     entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
     weight = models.IntegerField(null=False, blank=False)
     def __str__(self):
-        return str(self.entry.date)
+        return str(self.entry.user) + ' ' + str(self.entry.date)
