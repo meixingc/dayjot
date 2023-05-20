c845dd00945a29357516cff31a93931363976934
meixingc <realmeixing@gmail.com>
2023-05-20 15:50:07 -0400

    serializer edit to match new model + user password

--- dayjot_project/dayjot_backend/serializers.py
+++ dayjot_project/dayjot_backend/serializers.py
@@ -4,8 +4,15 @@ from .models import User, Entry, Water, Food, Exercise, Sleep, Weight
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name', 'picture', 'height', 'weight']
+        fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name', 'picture']
         extra_kwargs = {'password': {'write_only': True, 'required': True}}
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
 
 class EntrySerializer(serializers.ModelSerializer):
     water = serializers.SerializerMethodField()
