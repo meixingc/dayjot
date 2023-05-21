41a3ad66b953c0de0202a4bb7db292d7c1e3e8ae
meixingc <realmeixing@gmail.com>
2023-05-20 20:20:49 -0400

    entry data render

--- dayjot_project/dayjot_frontend/src/components/entry/Entry.js
+++ dayjot_project/dayjot_frontend/src/components/entry/Entry.js
@@ -1,5 +1,59 @@
+import '../../styles/Entry.css'
+
+import { useContext } from 'react'
+import { DataContext } from '../../context/DataContext'
+import { useNavigate } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+
 export default function Entry() {
-    return (
-        <h1> Entry </h1>
-    )
+    const navigate = useNavigate()
+
+    const { id } = useParams()
+    const { entries, waters, foods, exercises } = useContext(DataContext)
+
+    const entry = entries.find(entry => entry.id == id)
+    const water = waters.filter(water => water.entry == id)
+    const food = foods.filter(food => food.entry == id)
+    const exercise = exercises.filter(exercise => exercise.entry == id)
+
+    if (entry) {
+        return (
+            <div className='Entry'>
+                <h1 className='entry-date'> {entry.date} </h1>
+                <div>
+                    <h1> Waters </h1>
+                    {
+                        water.map(water => (
+                            <div>
+                                <h2> {water.amount} ml </h2>
+                            </div>
+                        ))
+                    }
+                </div>
+                <div>
+                    <h1> Foods </h1>
+                    {
+                        food.map(food => (
+                            <div>
+                                <h2> {food.name} </h2>
+                                <h2> {food.calories} calories </h2>
+                            </div>
+                        ))
+                    }
+                </div>
+                <div>
+                    <h1> Exercises </h1>
+                    {
+                        exercise.map(exercise => (
+                            <div>
+                                <h2> {exercise.name} </h2>
+                                <h2> {exercise.calories} calories </h2>
+                            </div>
+                        ))
+                    }
+                </div>
+                <button className='entry-edit' onClick={() => navigate(`/entry/edit/${id}`)}> Edit Entry </button>
+            </div>
+        )
+    }
 }
