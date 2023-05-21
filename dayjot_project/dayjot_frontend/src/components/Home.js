c33ce5074f89a114f22dee60e9f19527f797d128
meixingc <realmeixing@gmail.com>
2023-05-20 20:16:54 -0400

    home data render

--- dayjot_project/dayjot_frontend/src/components/Home.js
+++ dayjot_project/dayjot_frontend/src/components/Home.js
@@ -1,5 +1,36 @@
+import '../styles/Home.css'
+
+import { useContext } from 'react'
+import { UserContext } from '../context/UserContext'
+import { DataContext } from '../context/DataContext'
+import { useNavigate } from 'react-router-dom'
+
 export default function Home() {
-    return (
-        <div> Home </div>
-    )
+    const navigate = useNavigate()
+
+    const { loggedIn } = useContext(UserContext)
+    const { entries } = useContext(DataContext)
+
+    const selectEntry = (id) => {
+        navigate(`/entry/${id}`)
+    }
+
+    if (!loggedIn) {
+        return (
+            <div className='Home'>
+                <img src='/assets/lock.png' className='home-lock' onClick={() => navigate('/')}/>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className='Home'>
+                {entries.map(entry => (
+                    <div className='home-entry' onClick={() => selectEntry(`${entry.id}`)}>
+                        <h2 className='home-entry-date'> {entry.date} </h2>
+                    </div>
+                ))}
+            </div>
+        )
+    }
 }
